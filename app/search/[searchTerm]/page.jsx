f678696bd34b168d709799a09849722074262ca5
jossyahia <jossyahia@gmail.com>
2023-05-18 11:22:41 +0100

    ook

--- app/search/[searchTerm]/page.jsx
+++ app/search/[searchTerm]/page.jsx
@@ -1,6 +1,6 @@
 
-"use client"
-import Results from "./../components/Results";
+"use static"
+import Results from "@/components/Results";
 
 export default async function SearchPage({ params }) {
   const res = await fetch(
--- app/store/[id]/page.jsx
+++ app/store/[id]/page.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use static"
 import Image from "next/image";
 
 async function getMovie(_id) {
